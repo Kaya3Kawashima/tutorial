8e914eff5591a68d07fc29107bbc0d2ac066260d
nagatapote <hiroaki29p@icloud.com>
2021-12-06 20:48:19 +0900

    fix: ユーザー一覧とカテゴリー一覧のリンク作成

--- pages/users/list.vue
+++ pages/users/list.vue
@@ -1,3 +1,29 @@
 <template>
-  <hi>ユーザー一覧</hi>
+  <div>
+  <h1>ユーザー一覧</h1>
+  <ul>
+    <li v-for="i of 3" :key="i">
+      <nuxt-link :to="`/users/${i}`">ユーザー{{i}}</nuxt-link>
+    </li>
+  </ul>
+  <h1>カテゴリー一覧</h1>
+    <ul>
+    <li v-for="category of categories" :key="category.name">
+      <nuxt-link :to="`/category/${category.name}`">{{ category.name }}</nuxt-link>
+    </li>
+  </ul>
+  </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        categories: [
+          { name: "apple" },
+          { name: "banana" },
+          { name: "orange" },
+        ]
+      }
+    }
+  }
+</script>
