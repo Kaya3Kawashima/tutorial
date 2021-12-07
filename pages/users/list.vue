f08d29ef5e72c757adea1419e27d6b2fb361b092
user <user@user-no-MacBook-Air.local>
2021-12-07 23:52:13 +0900

    feat:todoリスト作成

--- pages/users/list.vue
+++ pages/users/list.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <hi>ユーザー一覧</hi>
+    <h1>ユーザー一覧</h1>
+    <ul>
+      <li v-for="i of 3" :key="i">
+        <nuxt-link :to="`/users/${i}`">ユーザー{{ i }}</nuxt-link>
+      </li>
+    </ul>
+    <h1>カテゴリー一覧</h1>
+    <ul>
+      <li v-for="category of categories" :key="category.name">
+        <nuxt-link :to="`/category/${category.name}`">{{
+          category.name
+        }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      categories: [{ name: "apple" }, { name: "banana" }, { name: "orange" }],
+    };
+  },
+};
+</script>
