b869ae0dadf2394d00ba674d470d04e58e47ad8f
user <user@user-no-MacBook-Air.local>
2021-12-15 22:22:40 +0900

    fix:todolistの変更

---
+++ components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="form">
+    <form @submit.prevent="todoAdd">
+      <label>やること</label>
+      <input v-model="todo" />
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: "",
+    };
+  },
+  methods: {
+    todoAdd() {
+      this.$emit("todoAdded", this.todo);
+      this.todo = "";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/todolist.vue
+++ pages/todolist.vue
@@ -1,54 +1,34 @@
 <template>
-  <div>
-    <h1>Todoリスト</h1>
-    <div class="addArea">
-      <input
-        type="text"
-        name="addName"
-        id="addName"
-        placeholder="タスクを入力してください"
-      />
-      <button id="addButton" class="button button--green">追加</button>
-    </div>
-    <div class="Filter">
-      <button class="button button--gray is-active">全て</button>
-      <button class="button button--gray">作業前</button>
-      <button class="button button--gray">作業中</button>
-      <button class="button button--gray">完了</button>
-    </div>
-    <table class="Lists">
-      <thead>
-        <tr>
-          <th>タスク</th>
-          <th>登録日時</th>
-          <th>状態</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in todos" :key="index">
-          <td>{{ item.content }}</td>
-          <td>{{ item.created }}</td>
-          <td>
-            <button class="button">{{ item.state }}</button>
-          </td>
-          <td><button class="button button--delete">削除</button></td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="page">
+    <progressbar :count="todos.length" :max="max" />
+    <Form @todoAdded="newTodo" />
+    <List class="todos" :todos="todos" />
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex';
+import List from "~/components/List.vue";
+import Form from "../components/Form.vue";
+import Progressbar from "../components/Progressbar.vue";
+
 export default {
-  data: function() {
-    return {
-      content:
-    }
+  components: {
+    List,
+    Form,
+    Progressbar,
   },
-  computed: {
-    ...mapState(['todos'])
-  }
-}
+  data() {
+    return {
+      todos: ["TODOを作る"],
+      max: 10,
+    };
+  },
+  methods: {
+    newTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+};
 </script>
+
+<style lang="scss"></style>
